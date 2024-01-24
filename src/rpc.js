584fe02d4e459e6af87191e5c3b9939c0f53fe0a
dzdidi <deniszalessky@gmail.com>
2024-01-24 12:00:14 +0000

    better debugs
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/rpc.js
+++ src/rpc.js
@@ -55,7 +55,7 @@ module.exports = class RPC {
       //   console.error('data:', JSON.stringify(data.toString()))
       //   outBuffer = Buffer.concat([outBuffer, data])
       // })
-      console.error('ARGS:', process.argv)
+      console.error('ARGS:', 'git', ['fetch', url, `${branch}:${branch}`], { env: { GIT_DIR: home.getCodePath(repo) } })
       let errBuffer = Buffer.from('')
       process.stderr.on('data', data => {
         errBuffer = Buffer.concat([errBuffer, data])
