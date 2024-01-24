c4b6be55274db4682dca30788bb03b8ce68e0dcb
dzdidi <deniszalessky@gmail.com>
2024-01-24 11:26:47 +0000

    fix buffer 3
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/rpc.js
+++ src/rpc.js
@@ -52,11 +52,11 @@ module.exports = class RPC {
     const outBuffer = Buffer.from('')
     const errBuffer = Buffer.from('')
     process.stdout.on('data', data => {
-      console.error('git data:', JSON.stringify(data.toString()))
+      console.error('data:', JSON.stringify(data.toString()))
       outBuffer.push(data)
     })
     process.stderr.on('data', data => {
-      console.error('git error:', JSON.stringify(data.toString()))
+      console.error('error:', JSON.stringify(data.toString()))
       errBuffer.push(data)
     })
 
